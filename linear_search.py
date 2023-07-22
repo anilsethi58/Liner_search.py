9ea1c2bf208896c5c36d0d09605ec8af397f5700
ANIL SETHI <136245167+anilsethi58@users.noreply.github.com>
2023-07-22 19:44:37 +0530

    Update linear_search.py
    Linear search in python is one way to find items in a list.

--- linear_search.py
+++ linear_search.py
@@ -1,20 +1,27 @@
 def search(arr, N, x):
   
     for i in range(0, N):
+      
         if (arr[i] == x):
             return i
     return -1
   
   
-# Driver Code
 if __name__ == "__main__":
+  
     arr = [2, 3, 4, 10, 40,23,55,65,77,100,91,38]
+  
     x=int(input("Enter the number from the list : "))
+  
     N = len(arr)
   
-    # Function call
+    #  call the function
+  
     output = search(arr, N, x)
     if(output == -1):
+      
         print("Element is not present in array")
+      
     else:
+      
         print("Element is present at index",output)
